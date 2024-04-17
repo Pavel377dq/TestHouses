971f6584d8c890b63d1215b9ffb9192ade89ce1e
Pavel377dq <pad93765@gmail.com>
2024-04-17 22:46:34 +0300

    second try

--- src/components/popUp/popUp.js
+++ src/components/popUp/popUp.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import close from "../../images/close.svg";
 import "./popUp.scss";
 
@@ -9,10 +9,30 @@ function PopUp({ setData, number, data, setOpen }) {
     porch: null,
     aparts: [],
   });
+  const [isDisableds, setIsDisableds] = useState([false, false, false, false, false, false]);
 
-  const [activeListIndex, setActiveListIndex] = useState(0);
-  const [activeElementList, setActiveElementList] = useState(0);
+  const pointersMenu= [0,-1];
+  
+  const addButton = useRef(null);
+  const listRefsLeft = [
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null)
+  ];
 
+  const listRefsRight = [
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null)
+  ];
+
+  
   const handleKeyDown = (e) => {
     if (
       e.key === "ArrowLeft" ||
@@ -25,30 +45,55 @@ function PopUp({ setData, number, data, setOpen }) {
 
     if (e.key === "ArrowLeft") {
       // Перемещение к предыдущему списку
-      setActiveListIndex((prevIndex) => (prevIndex === 0 ? 1 : prevIndex - 1));
+      listRefsLeft[0].current.focus();
+      pointersMenu[0] = 0;
+      pointersMenu[1] = -1;
+    
     } else if (e.key === "ArrowRight") {
       // Перемещение к следующему списку
-      setActiveListIndex((prevIndex) => (prevIndex === 1 ? 0 : prevIndex + 1));
+
+      listRefsRight[0].current.focus();
+      pointersMenu[1] = 0;
+      pointersMenu[0] = -1;
+
     } else if (e.key === "ArrowUp") {
-      // Перемещение фокуса вверх по текущему списку
-      setActiveElementList((prevIndex) => Math.max(prevIndex - 1, 0));
+        console.log(pointersMenu,'pointersMenu')
+        if(pointersMenu[0] !== -1){//фокус на левом меню
+            listRefsLeft[Math.max(pointersMenu[0] - 1, 0)].current.focus();
+            pointersMenu[0] = Math.max(pointersMenu[0] - 1, 0);
+        }
+        else{// фокус на правом меню
+            listRefsRight[Math.max(pointersMenu[1] - 1, 0)].current.focus();
+            pointersMenu[1] = Math.max(pointersMenu[1] - 1, 0);
+        }
+        
+      
     } else if (e.key === "ArrowDown") {
-      // Перемещение фокуса вниз по текущему списку
-      setActiveElementList((prevIndex) => (prevIndex + 1) % 6);
+
+        if(pointersMenu[0] !== -1){//фокус на левом меню
+            listRefsLeft[(pointersMenu[0] + 1) % 6].current.focus();
+            pointersMenu[0] = (pointersMenu[0] + 1) % 6;
+        }
+        else{
+            listRefsRight[(pointersMenu[1] + 1) % 6].current.focus();
+            pointersMenu[1] = (pointersMenu[1] + 1) % 6;
+        }
     }
+    else if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+        //e.preventDefault();
+       addButton.current?.click();
+       
+      }
   };
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeyDown);
+    listRefsLeft[0].current.focus();
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
 
-  const handleItemClick = (listIndex, itemIndex) => {
-    setActiveListIndex(listIndex);
-    setActiveElementList(itemIndex);
-  };
 
   function addPorch(num) {
     const aparts = [];
@@ -77,11 +122,15 @@ function PopUp({ setData, number, data, setOpen }) {
 
   function add() {
     const newData = JSON.parse(JSON.stringify(data));
-    console.log(newData, "newData", number, "number");
-    newData[number].porchAparts.push({
+
+    // Получаем текущее состояние addedElements
+    const currentAddedElements = {
       porch: addedElements.porch,
       aparts: [...addedElements.aparts],
-    });
+    };
+  
+    newData[number].porchAparts.push(currentAddedElements);
+  
     setData(newData);
     setOpen({
       isOpen: false,
@@ -108,17 +157,15 @@ function PopUp({ setData, number, data, setOpen }) {
         {porches.map((num, idx) => (
           <li
             tabIndex={0}
-            className={
-              activeListIndex === 0 && activeElementList === idx
-                ? "popUp__element_selected popUp__element"
-                : "popUp__element"
-            }
+            onKeyDown={(e)=>{if(e.key === 'Enter') {  addPorch(num);
+            }}}
+            ref={listRefsLeft[idx]}
+            className={ "popUp__element"}
           >
             <button
               className="popUp__button"
               onClick={() => {
                 addPorch(num);
-                handleItemClick(0, num - 1);
               }}
             >
               porch {num}
@@ -143,17 +190,26 @@ function PopUp({ setData, number, data, setOpen }) {
           </li>
           {apartments.map((num, idx) => (
             <li
-              className={
-                activeListIndex === 1 && activeElementList === idx
-                  ? "popUp__element_selected popUp__element"
-                  : "popUp__element"
-              }
+            tabIndex={0}
+            onKeyDown={(e)=>{if((e.key === 'Enter') && !(e.ctrlKey || e.metaKey)) { 
+                addApart(num);
+                const isDisblds = [...isDisableds];
+                isDisblds[idx] = true;
+              setIsDisableds(isDisblds);
+            }}}
+            ref={listRefsRight[idx]}
+
+              className={ isDisableds[idx] ? "popUp__element popUp__element_disabled": "popUp__element"  }
+
+              
             >
               <button
                 className="popUp__button"
                 onClick={() => {
                   addApart(num);
-                  handleItemClick(1, num - 1);
+                  const isDisblds = [...isDisableds];
+                  isDisblds[idx] = true;
+                setIsDisableds(isDisblds);
                 }}
               >
                 apartment {num}
@@ -161,7 +217,7 @@ function PopUp({ setData, number, data, setOpen }) {
             </li>
           ))}
           <li>
-            <button className="popUp__button" onClick={add}>
+            <button className="popUp__button" ref={addButton}onClick={() =>add()}>
               Add
             </button>
           </li>
